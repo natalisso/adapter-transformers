13f266cfdbf828914e172ed57802e8dcc55a557f
Natália Soares <nataliasoares@Natalias-MacBook-Pro.local>
2023-08-04 00:19:32 +0200

    Change default sentencepiece extract

--- src/transformers/convert_slow_tokenizer.py
+++ src/transformers/convert_slow_tokenizer.py
@@ -40,22 +40,49 @@ class SentencePieceExtractor:
         self.sp = SentencePieceProcessor()
         self.sp.Load(model)
 
-    def extract(self) -> Tuple[Dict[str, int], List[Tuple]]:
+    # def extract(self) -> Tuple[Dict[str, int], List[Tuple]]:
+    #     sp = self.sp
+    #     vocab = {sp.id_to_piece(index): index for index in range(sp.GetPieceSize())}
+
+    #     # Merges
+    #     merges = []
+    #     for piece_l in vocab.keys():
+    #         for piece_r in vocab.keys():
+    #             merge = f"{piece_l}{piece_r}"
+    #             piece_id = vocab.get(merge, None)
+    #             if piece_id:
+    #                 merges += [(piece_l, piece_r, piece_id)]
+    #     merges = sorted(merges, key=lambda val: val[2])
+    #     merges = [(val[0], val[1]) for val in merges]
+
+    #     return vocab, merges
+    def extract(self, vocab_scores=None) -> Tuple[Dict[str, int], List[Tuple]]:
+        """
+        By default will return vocab and merges with respect to their order, by sending `vocab_scores` we're going to
+        order the merges with respect to the piece scores instead.
+        """
         sp = self.sp
         vocab = {sp.id_to_piece(index): index for index in range(sp.GetPieceSize())}
+        if vocab_scores is not None:
+            vocab_scores, reverse = dict(vocab_scores), True
+        else:
+            vocab_scores, reverse = vocab, False
 
         # Merges
         merges = []
-        for piece_l in vocab.keys():
-            for piece_r in vocab.keys():
-                merge = f"{piece_l}{piece_r}"
-                piece_id = vocab.get(merge, None)
-                if piece_id:
-                    merges += [(piece_l, piece_r, piece_id)]
-        merges = sorted(merges, key=lambda val: val[2])
-        merges = [(val[0], val[1]) for val in merges]
+        for merge, piece_score in vocab_scores.items():
+            local = []
+            for index in range(1, len(merge)):
+                piece_l, piece_r = merge[:index], merge[index:]
+                if piece_l in vocab and piece_r in vocab:
+                    local.append((piece_l, piece_r, piece_score))
+            local = sorted(local, key=lambda x: (vocab[x[0]], vocab[x[1]]))
+            merges.extend(local)
 
+        merges = sorted(merges, key=lambda val: val[2], reverse=reverse)
+        merges = [(val[0], val[1]) for val in merges]
         return vocab, merges
+
 
 
 def check_number_comma(piece: str) -> bool:
